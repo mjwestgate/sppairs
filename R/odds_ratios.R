516c061c4455a2e544d4cfd9b4c61211325d5c00
mjwestgate <martinwestgate@Martins-MacBook-Pro.local>
2014-08-01 11:35:04 +0100

    update odds ratio calculation
    Deconstruct glm and glmer approaches into separate functions;
    rename pairwise.odds.ratios() to spaa();
    change spaa() behaviour to allow OR specification using ‘method’;
    clean object returned by spaa()

--- R/odds_ratios.R
+++ R/odds_ratios.R
@@ -1,5 +1,5 @@
 # Simple, internal function to check input datasets for compatability with later functions in library(sppairs)
-odds.ratio.test<-function(dataset)
+or.check<-function(dataset)
 {
 if(dim(dataset)[2]!=2){stop("input does not have two columns")}		# check size
 test<-as.character(apply(dataset, 2, class))		# check contains numbers
@@ -13,7 +13,7 @@ invisible(dataset)		# return the (possibly corrected) dataset for use in later f
 # Calculate asymmetric odds ratio from a contingency table
 or.contingency<-function(dataset)	
 {
-dataset<-odds.ratio.test(dataset)		# will either correct the dataset, or stop this function with an error
+dataset<-or.check(dataset)		# will either correct the dataset, or stop this function with an error
 cont.table <-as.matrix(table(dataset))[2:1, 2:1]
 c<-cont.table[1, 1]; d<-cont.table[1, 2]; e<-cont.table[2, 1]; f<-cont.table[2, 2]
 odds.ratio<-(c/d) / ( (c+e) / (d+f) )
@@ -21,44 +21,61 @@ return(odds.ratio)
 }
 
 
-# Odds ratio calculation using regression
-or.regression<-function(dataset, random.effect)
+# Calculate odds ratio from results of glm or glmer. Internal to or.glmer and or.glm
+or.regression<-function(b, z0, z1)
 {
-dataset<-odds.ratio.test(dataset)		# will either correct the dataset, or stop this function with an error
-b<-(1/dim(dataset)[1])*sum(dataset[, 2])		# proportion of sites at which sp. B occurred
-require(boot)	# for logit() & inv.logit
-
-# To call or.regression without random effects (note this should give similar results to or.contingency()):
-if(missing(random.effect))	
-	{
-	model0<-glm(dataset[, 1]~1, family=binomial(link="logit"))
-	model1<-glm(dataset[, 1]~dataset[, 2], family=binomial(link="logit"))
-	z0<-as.numeric(coef(model0)[1])	# intercept; occurrence of sp. A in the absence of sp. B
-	z1<-sum(as.numeric(coef(model1)))	# covariate; occurrence of sp. A in the presence of sp. B
-# Alternatively (i.e. if random effects are specified), call glmer(lme4)
-}else{
-	require(lme4)
-	model0<-glmer(dataset[, 1]~1 + (1|random.effect), family=binomial(link="logit"))
-	model1<-glmer(dataset[, 1]~dataset[, 2] + (1|random.effect), family=binomial(link="logit"))
-	z0<-as.numeric(fixef(model0))
-	z1<-sum(as.numeric(fixef(model1)))
-}	# end else
-  
+library(boot)	# for logit() & inv.logit
 # regardless of the method used, calculate the odds ratio
 odds.ratio<-exp( z1- logit(
 	(b * inv.logit(z1)) +
 	((1-b) * inv.logit(z0))
 )	# end logit
 )	# end exp
-  
 return(odds.ratio)
-} 
+}
+
+
+# Odds ratio calculation using lme4
+or.glmer<-function(dataset, random.effect)
+{
+dataset<-or.check(dataset)		# will either correct the dataset, or stop this function with an error
+b<-(1/dim(dataset)[1])*sum(dataset[, 2])		# proportion of rows at which sp. B occurred
+library(lme4)
+model0<-glmer(dataset[, 1]~1 + (1|random.effect), family=binomial(link="logit"))
+model1<-glmer(dataset[, 1]~dataset[, 2] + (1|random.effect), family=binomial(link="logit"))
+z0<-as.numeric(fixef(model0))
+z1<-sum(as.numeric(fixef(model1)))
+odds.ratio<-or.regression(b, z0, z1)
+return(odds.ratio)
+}
+
+
+# Odds ratio calculation using glm
+or.glm<-function(dataset)
+{
+dataset<-or.check(dataset)		# will either correct the dataset, or stop this function with an error
+b<-(1/dim(dataset)[1])*sum(dataset[, 2])		# proportion of rows at which sp. B occurred
+model0<-glm(dataset[, 1]~1, family=binomial(link="logit"))
+model1<-glm(dataset[, 1]~dataset[, 2], family=binomial(link="logit"))
+z0<-as.numeric(coef(model0)[1])	# intercept; occurrence of sp. A in the absence of sp. B
+z1<-sum(as.numeric(coef(model1)))	# covariate; occurrence of sp. A in the presence of sp. B
+odds.ratio<-or.regression(b, z0, z1)
+return(odds.ratio)
+}
 
 
 # Pairwise odds ratio calculation
-pairwise.odds.ratios<-function(dataset, random.effect, rarity.cutoff, quiet)
+spaa<-function(dataset, method, rarity.cutoff, random.effect)
 {
-if(missing(quiet))quiet<-FALSE
+
+# set some default behaviour
+if(missing(method)){
+	if(missing(random.effect)){method<-"contingency"}else{method<-"glmer"}
+}else{	# i.e. is method is specified
+	if(method=="glmer" & missing(random.effect)){
+	method <-"contingency"
+	cat("Warning: method 'glmer' not available without specifying 'random.effect'. Switched to method 'contingency'")}
+}
 if(missing(rarity.cutoff))rarity.cutoff<-0.1
 if(any(dataset>1)){dataset<-make.binary(dataset)}		# check if any values >1; if so, convert to binary
   
@@ -69,9 +86,10 @@ dataset<-dataset[, which(occu.result>rarity.cutoff)]
 # create an object to export frequency information
 frequency.result<-occu.result[which(occu.result>rarity.cutoff)]
 frequency.result<-data.frame(
-	species=names(frequency.result),
-	value=as.numeric(frequency.result))
-frequency.result$species<-as.character(frequency.result$species)
+	name=names(frequency.result),
+	frequency=as.numeric(frequency.result),
+	stringsAsFactors=FALSE)
+#frequency.result$species<-as.character(frequency.result$species)
   
 # create combn of remaining species
 n.species<-dim(dataset)[2]
@@ -82,30 +100,33 @@ combinations.final<-data.frame(
 	col2=combinations[, 2],
 	sp1=colnames(dataset)[combinations[, 1]],
 	sp2=colnames(dataset)[combinations[, 2]],
-	odds=rep(NA, dim(combinations)[1]))
+	odds=rep(NA, dim(combinations)[1]),
+	stringsAsFactors=FALSE)
 combinations.final$odds<-as.numeric(combinations.final$odds)
-  
-# run odds.ratio calculation for each pair
-if(quiet==FALSE){
-	n.rows<-dim(combinations.final)[1]
-	cat(paste("calculating", n.rows, "combinations of", n.species, "species.", sep=" "))}
+n.rows<-dim(combinations.final)[1]
 
 # run analysis in loop
-for(i in 1:dim(combinations.final)[1]){
+for(i in 1: n.rows){
 	dataset.thisrun<-dataset[, c(combinations.final$col1[i], combinations.final$col2[i])]
 	or.simple<-or.contingency(dataset.thisrun)	# run as a test: req. as glmer will fail if no overlap in presences.
-	if(missing(random.effect)){combinations.final$odds[i]<-or.simple
-	}else{
-		if(any(c(0, Inf)==or.simple)){combinations.final$odds[i]<-or.simple
-		}else{combinations.final$odds[i]<-or.regression(dataset.thisrun, random.effect)}
-	}	# end if(random effect specified)
-}	# end loop (i.e. all combinations of spp.)
+	switch(method,
+		"contingency"={combinations.final$odds[i]<-or.simple},
+		"glm"={if(any(c(0, Inf)==or.simple)){combinations.final$odds[i]<-or.simple
+			}else{combinations.final$odds[i]<-or.glm(dataset.thisrun)}},
+		"glmer"={if(any(c(0, Inf)==or.simple)){combinations.final$odds[i]<-or.simple
+			}else{combinations.final$odds[i]<-or.glmer(dataset.thisrun, random.effect)}})
+	}
   
 # create and return objects necessary for plotting
 output<-list(
-	frequency=frequency.result,
-	result.long=combinations.final,
-	result.wide=pairwise.or.matrix(combinations.final))
+	values=list(
+		method=method,
+		observations=dim(dataset)[1],
+		rarity.cutoff= rarity.cutoff,
+		species= n.species,
+		combinations= n.rows),
+	species=frequency.result,
+	combinations=combinations.final)
 
 return(output)
 }
